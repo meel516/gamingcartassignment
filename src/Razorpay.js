62bb891449248fd52ea36a6b86e1f266b61ae725
meel516 <md.saleem516@gmail.com>
2023-07-24 10:38:49 +0530

    changed name

--- src/Razorpay.js
+++ src/Razorpay.js
@@ -4,7 +4,7 @@ export var options = {
     key: 'rzp_test_0xajL4l1kfjBxJ', // Enter the Key ID generated from the Dashboard
     amount: 10000, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
     currency: "INR",
-    name: "Acme Corp",
+    name: "free4all",
     description: "Test Transaction",
     image: "",
     order_id: 'order_MHTGwgj54ivKmL', //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
